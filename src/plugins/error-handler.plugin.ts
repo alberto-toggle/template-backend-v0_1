869d44ffd23a98501a8bd0c806638dd49d9bd73d
Hector Gomez <hemagome-28@hotmail.com>
2026-02-05 16:45:04 -0600

    chore: npm run format applied

--- src/plugins/error-handler.plugin.ts
+++ src/plugins/error-handler.plugin.ts
@@ -1,14 +1,12 @@
 import type { FastifyError, FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
 export async function registerErrorHandler(fastify: FastifyInstance) {
-  fastify.setErrorHandler(
-    (error: FastifyError, _request: FastifyRequest, reply: FastifyReply) => {
-      const statusCode = error.statusCode ?? 500;
-      reply.status(statusCode).send({
-        error: error.name,
-        message: error.message,
-        statusCode
-      });
-    }
-  );
+  fastify.setErrorHandler((error: FastifyError, _request: FastifyRequest, reply: FastifyReply) => {
+    const statusCode = error.statusCode ?? 500;
+    reply.status(statusCode).send({
+      error: error.name,
+      message: error.message,
+      statusCode
+    });
+  });
 }
